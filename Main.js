2ffe1668161c61622c97cc94275e8bac2e91d5d0
EliedLr <78989591+EliedLr@users.noreply.github.com>
2021-12-21 22:01:41 -0500

    Main.js

---
+++ Main.js
@@ -0,0 +1,8 @@
+if(confirm ("Quieres ser Doxeado? guiño guiño")){
+    alert ("BUEN CHICO *LO BESA* XD")
+}else{
+    alert ("Me desepcionas")
+    window.location.href="https://youtu.be/dQw4w9WgXcQ"
+        
+    
+}
